92d8120012caa15bb7366b65b9bfddd5253c8e79
@hexlet/cli <support@hexlet.io>
2022-12-19 13:15:39 +0400

    submit java-web-ru/tests

---
+++ java-web-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,125 @@
+package exercise;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import io.javalin.Javalin;
+import io.ebean.DB;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+import io.ebean.Database;
+
+class AppTest {
+
+    private static Javalin app;
+    private static String baseUrl;
+
+    // BEGIN
+    @BeforeAll
+    static void beforeAll() {
+        app = App.getApp();
+        app.start();
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        app.stop();
+    }
+    // END
+
+    // Между тестами база данных очищается
+    // Благодаря этому тесты не влияют друг на друга
+    @BeforeEach
+    void beforeEach() {
+        Database db = DB.getDefault();
+        db.truncate("user");
+        User existingUser = new User("Wendell", "Legros", "a@a.com", "123456");
+        existingUser.save();
+    }
+
+    @Test
+    void testUsers() {
+
+        // Выполняем GET запрос на адрес http://localhost:port/users
+        HttpResponse<String> response = Unirest
+            .get(baseUrl + "/users")
+            .asString();
+        // Получаем тело ответа
+        String content = response.getBody();
+
+        // Проверяем код ответа
+        assertThat(response.getStatus()).isEqualTo(200);
+        // Проверяем, что страница содержит определенный текст
+        assertThat(response.getBody()).contains("Wendell Legros");
+    }
+
+    @Test
+    void testNewUser() {
+
+        HttpResponse<String> response = Unirest
+            .get(baseUrl + "/users/new")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    // BEGIN
+    @Test
+    void testCreateUserPositive() {
+        String firstName = "Anastasiia";
+        String lastName = "Mineeva";
+        String email = "nastya@mail.ru";
+        String password = "1234";
+        HttpResponse<String> response = Unirest
+                .post(baseUrl + "/users")
+                .field("firstName", firstName)
+                .field("lastName", lastName)
+                .field("email", email)
+                .field("password", password)
+                .asString();
+        assertThat(response.getStatus()).isEqualTo(302);
+        User user = new QUser()
+                .lastName.equalTo(lastName)
+                .findOne();
+        assertThat(user).isNotNull();
+        assertThat(user.getFirstName()).isEqualTo(firstName);
+        assertThat(user.getEmail()).isEqualTo(email);
+        assertThat(user.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    void testCreateUserNegative() {
+        String firstName = "";
+        String lastName = "";
+        String email = "nastya";
+        String password = "12";
+        HttpResponse<String> response = Unirest
+                .post(baseUrl + "/users")
+                .field("firstName", firstName)
+                .field("lastName", lastName)
+                .field("email", email)
+                .field("password", password)
+                .asString();
+
+        User user = new QUser()
+                .lastName.equalTo(lastName)
+                .findOne();
+
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(422);
+        assertThat(user).isNull();
+        assertThat(body).contains("12");
+        assertThat(body).contains("nastya");
+    }
+    // END
+}
